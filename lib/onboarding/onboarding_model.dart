0434636dd5c549df8ed9fae18c5bc559301403c7
MelnickJ24 <jmelnick15@gmail.com>
2025-10-07 13:42:42 -0400

    quotes

---
+++ lib/onboarding/onboarding_model.dart
@@ -0,0 +1,49 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/backend/firebase_storage/storage.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/upload_data.dart';
+import 'dart:ui';
+import '/index.dart';
+import 'onboarding_widget.dart' show OnboardingWidget;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:easy_debounce/easy_debounce.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class OnboardingModel extends FlutterFlowModel<OnboardingWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  bool isDataUploading_profilePhoto = false;
+  FFUploadedFile uploadedLocalFile_profilePhoto =
+      FFUploadedFile(bytes: Uint8List.fromList([]));
+  String uploadedFileUrl_profilePhoto = '';
+
+  // State field(s) for Name widget.
+  FocusNode? nameFocusNode;
+  TextEditingController? nameTextController;
+  String? Function(BuildContext, String?)? nameTextControllerValidator;
+  // State field(s) for userName widget.
+  FocusNode? userNameFocusNode;
+  TextEditingController? userNameTextController;
+  String? Function(BuildContext, String?)? userNameTextControllerValidator;
+  DateTime? datePicked;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    nameFocusNode?.dispose();
+    nameTextController?.dispose();
+
+    userNameFocusNode?.dispose();
+    userNameTextController?.dispose();
+  }
+}
